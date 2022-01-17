491f961acd0efb0cc5a460646c6968d0b0d2610d
Ukwosah-Chilenwa-215672629 <chilenwa@my.yorku.ca>
2022-01-17 04:07:12 -0500

    first Commit

---
+++ src/dictionary.js
@@ -0,0 +1,17 @@
+const Keys = {
+    product: 'https://www.supremenewyork.com/shop/accessories/z4yis9z7c',
+    name: "Solomon Ukwosah",
+    email: 'solomonukwosah@gmail.com',
+    address: '33 Coltrane Dr ',
+    zip: 'L4J9A9',
+    tel: 4379727821,
+    card_number: 5191230200260629,
+    cvv: 315,
+    exp_month: 03,
+    exp_year: 2023,
+
+
+}
+// sizing 
+//*[@id="s"]/option[1]
+module.exports.Keys = Keys;
